eb3121a4bd60ac08c0e7ed9d6e8b751e32b5023a
Vlad Ponomarov <vladyslav.ponomarov@workday.com>
2023-04-04 13:43:31 +0200

    Correct chess task

--- lesson_1/chess.py
+++ lesson_1/chess.py
@@ -1,151 +1,116 @@
+from typing import Any, List
+
+
+def _check_if_in_board(new_place: tuple) -> bool:
+    if all(0 < place <= 8 for place in new_place):
+        return True
+    else:
+        print('Cannot use this place')
+        return False
+
+
 class Figure:
-    color = "white"
-    place_board = (1, 1)
-    move = (1, 1)
-    board = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8),
-             (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8),
-             (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8),
-             (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8),
-             (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8),
-             (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8),
-             (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8),
-             (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7), (8, 8),
-             ]
+    _color: str = "white"
+    _place: tuple = (1, 1)
 
-    def info(self):
-        return f'place_board is {self.place_board} and color is {self.color}'
+    def info(self) -> str:
+        return f'place_board is {self._place} and color is {self._color}'
 
-    def change_color(self):
-        if self.color == "white":
-            self.color = "black"
-        elif self.color == "black":
-            self.color = "white"
-        return self.color
+    def change_color(self) -> None:
+        if self._color == "white":
+            self._color = "black"
+        elif self._color == "black":
+            self._color = "white"
 
-    def change_place(self):
-        if self.place_board in self.board:
-            return True
-        else:
-            return False
+    def change_place(self, new_place: tuple) -> None:
+        if _check_if_in_board(new_place):
+            self._place = new_place
+            print(f'Figure {self.__class__.__name__} is moved to {new_place}')
 
-    def move_pawn(self):
-        if self.color == "white":
-            (a, b) = self.place_board
-            new_pl = (a-1, b)
-            if new_pl == self.move:
-                return "you can move your`s {} pawn from {} to {}".format(self.color, self.place_board, self.move)
-            else:
-                return "try another move"
-        if self.color == "black":
-            (a, b) = self.place_board
-            new_pl = (a+1, b)
-            if new_pl == self.move:
-                return "you can move your`s {} pawn from {} to {}".format(self.color, self.place_board, self.move)
-            else:
-                return "try another move"
-
-    def move_king(self):
-        (a, b) = self.place_board
-        new_pl = [(a + 1, b), (a + 1, b + 1), (a + 1, b - 1),
-                  (a, b + 1), (a, b - 1),
-                  (a - 1, b), (a - 1, b + 1), (a - 1, b - 1)]
-        if self.move in new_pl:
-            return "you can move your`s King from {} to {}".format(self.place_board, self.move)
-        else:
-            return "try another move"
-
-    def move_castle(self):
-        (a, b) = self.place_board
-        new_pl = [(a + 1, b), (a + 2, b), (a + 3, b), (a + 4, b), (a + 5, b), (a + 6, b), (a + 7, b),
-                  (a, b + 1), (a, b + 2), (a, b + 3), (a, b + 4), (a, b + 5), (a, b + 6), (a, b + 7),
-                  (a - 1, b), (a - 2, b), (a - 3, b), (a - 4, b), (a - 5, b), (a - 6, b), (a - 7, b),
-                  (a, b - 1), (a, b - 2), (a, b - 3), (a, b - 4), (a, b - 5), (a, b - 6), (a, b - 7),
-                  ]
-        if self.move in new_pl:
-            return "you can move your`s Castle from {} to {}".format(self.place_board, self.move)
-        else:
-            return "try another move"
-
-    def move_bishop(self):
-        (a, b) = self.place_board
-        new_pl = [
-            (a + 1, b - 1), (a + 2, b - 2), (a + 3, b - 3), (a + 4, b - 4), (a + 5, b - 5), (a + 6, b - 6),
-            (a + 7, b - 7),
-            (a - 1, b + 1), (a - 2, b + 2), (a - 3, b + 3), (a - 4, b + 4), (a - 5, b + 5), (a - 6, b + 6),
-            (a - 7, b + 7),
-            (a - 1, b + 1), (a - 2, b + 2), (a - 3, b + 3), (a - 4, b + 4), (a - 5, b + 5), (a - 6, b + 6),
-            (a - 7, b + 7),
-            (a + 1, b - 1), (a + 2, b - 2), (a + 3, b - 3), (a + 4, b - 4), (a + 5, b - 5), (a + 6, b - 6),
-            (a + 7, b - 7)
-            ]
-        if self.move in new_pl:
-            return "you can move your`s Bishop from {} to {}".format(self.place_board, self.move)
-        else:
-            return "try another move"
-
-    def move_queen(self):
-        (a, b) = self.place_board
-        new_pl = [
-            (a + 1, b - 1), (a + 2, b - 2), (a + 3, b - 3), (a + 4, b - 4), (a + 5, b - 5), (a + 6, b - 6),
-            (a + 7, b - 7),
-            (a - 1, b + 1), (a - 2, b + 2), (a - 3, b + 3), (a - 4, b + 4), (a - 5, b + 5), (a - 6, b + 6),
-            (a - 7, b + 7),
-            (a - 1, b + 1), (a - 2, b + 2), (a - 3, b + 3), (a - 4, b + 4), (a - 5, b + 5), (a - 6, b + 6),
-            (a - 7, b + 7),
-            (a + 1, b - 1), (a + 2, b - 2), (a + 3, b - 3), (a + 4, b - 4), (a + 5, b - 5), (a + 6, b - 6),
-            (a + 7, b - 7),
-            (a + 1, b), (a + 2, b), (a + 3, b), (a + 4, b), (a + 5, b), (a + 6, b), (a + 7, b),
-            (a, b + 1), (a, b + 2), (a, b + 3), (a, b + 4), (a, b + 5), (a, b + 6), (a, b + 7),
-            (a - 1, b), (a - 2, b), (a - 3, b), (a - 4, b), (a - 5, b), (a - 6, b), (a - 7, b),
-            (a, b - 1), (a, b - 2), (a, b - 3), (a, b - 4), (a, b - 5), (a, b - 6), (a, b - 7),
-        ]
-        if self.move in new_pl:
-            return "you can move your`s Queen from {} to {}".format(self.place_board, self.move)
-        else:
-            return "try another move"
-
-    def move_knight(self):
-        (a, b) = self.place_board
-        new_pl = [(a + 1, b - 2), (a + 2, b - 1), (a - 1, b + 2), (a - 2, b + 1),
-                  (a + 1, b + 2), (a + 2, b + 1), (a - 1, b - 2), (a - 2, b - 1)]
-        if self.move in new_pl:
-            return "you can move your`s Knight from {} to {}".format(self.place_board, self.move)
-        else:
-            return "try another move"
+    def check_move(self, new_place: tuple) -> Any:
+        if _check_if_in_board(new_place):
+            raise NotImplementedError
 
 
-king = Figure()
-king.place_board = (1, 1)
-king.color = "black"
-king.move = (3, 3)
+class Pawn(Figure):
+    def check_move(self, new_place: tuple) -> bool:
+        old_horizontal, old_vertical = self._place
+        new_horizontal, new_vertical = new_place
+        return (self._color == "white" and new_vertical == old_vertical + 1 and new_horizontal == old_horizontal) or (
+                    self._color == "black" and new_vertical == old_vertical - 1 and new_horizontal == old_horizontal)
 
-queen = Figure()
-queen.place_board = (1, 8)
-queen.color = "black"
-queen.move = (1, 3)
 
-castle = Figure()
-castle.place_board = (1, 8)
-castle.color = "black"
-castle.move = (8, 1)
+class King(Figure):
+    def check_move(self, new_place: tuple) -> bool:
+        old_horizontal, old_vertical = self._place
+        new_horizontal, new_vertical = new_place
+        return abs(new_horizontal - old_horizontal) <= 1 and abs(new_vertical - old_vertical) <= 1
 
-bishop = Figure()
-bishop.place_board = (1, 8)
-bishop.color = "black"
-bishop.move = (8, 1)
 
-knight = Figure()
-knight.place_board = (1, 8)
-knight.color = "black"
-knight.move = (2, 6)
+class Rook(Figure):
+    def check_move(self, new_place: tuple) -> bool:
+        old_horizontal, old_vertical = self._place
+        new_horizontal, new_vertical = new_place
+        return old_vertical == new_vertical or old_horizontal == new_horizontal
 
-pawn = Figure()
-pawn.place_board = (1, 8)
-pawn.color = "black"
-pawn.move = (1, 2)
 
-print(king.change_color())
-print(king.change_place())
-print(king.info())
-print(king.move_king())
-print(knight.move_knight())
+class Bishop(Figure):
+    def check_move(self, new_place: tuple) -> bool:
+        old_horizontal, old_vertical = self._place
+        new_horizontal, new_vertical = new_place
+        return abs(new_vertical - old_vertical) == abs(new_horizontal - old_horizontal)
+
+
+class Queen(Figure):
+    def check_move(self, new_place: tuple) -> bool:
+        old_horizontal, old_vertical = self._place
+        new_horizontal, new_vertical = new_place
+        as_bishop = abs(new_vertical - old_vertical) == abs(new_horizontal - new_horizontal)
+        as_rook = old_vertical == new_vertical or old_horizontal == new_horizontal
+        return as_rook or as_bishop
+
+
+class Knight(Figure):
+    def check_move(self, new_place: tuple) -> bool:
+        old_horizontal, old_vertical = self._place
+        new_horizontal, new_vertical = new_place
+        vertical_diff = abs(old_vertical - new_vertical)
+        horizontal_diff = abs(old_horizontal - new_horizontal)
+        return (vertical_diff == 1 and horizontal_diff == 2) or (vertical_diff == 2 and horizontal_diff == 1)
+
+
+def get_figures_which_can_move(figures_to_check: List[Figure], new_place: tuple) -> List[Figure]:
+    return [figure for figure in figures_to_check if figure.check_move(new_place)]
+
+
+pawn1 = Pawn()
+pawn2 = Pawn()
+pawn3 = Pawn()
+king1 = King()
+king2 = King()
+king3 = King()
+rook1 = Rook()
+rook2 = Rook()
+rook3 = Rook()
+bishop1 = Bishop()
+bishop2 = Bishop()
+bishop3 = Bishop()
+queen1 = Queen()
+queen2 = Queen()
+queen3 = Queen()
+knight1 = Knight()
+knight2 = Knight()
+knight3 = Knight()
+
+pawn1.change_place((3, 3))
+king2.change_place((4, 4))
+bishop2.change_place((6, 1))
+knight3.change_place((5, 5))
+queen1.change_place((3, 7))
+pawn2.change_color()
+pawn2.change_place((3, 5))
+
+figures = [pawn1, pawn2, pawn3, queen1, queen2, queen3, king1, king2, king3, bishop1, bishop2, bishop3, rook1, rook2,
+           rook3, knight1, knight2, knight3]
+
+result = get_figures_which_can_move(figures, (3,4))
