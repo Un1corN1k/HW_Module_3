555202505144ca3a17eaeb40a6c135342965b7ca
Oleksandr Yeromin <127413390+Un1corN1k@users.noreply.github.com>
2023-04-12 18:03:24 +0300

    fix problems,all tasks is done

--- lesson_2/store.py
+++ lesson_2/store.py
@@ -4,16 +4,13 @@ class ShoppingCart:
         self.quantities = []
 
     def __bool__(self):
-        if self.products == [] and self.quantities == []:
-            return False
-        else:
-            return True
+        return bool(self.products)
 
     def __eq__(self, other):
         return self.products == other.products and self.quantities == other.quantities
 
     def __ne__(self, other):
-        return self.products != other.products and self.quantities != other.quantities
+        return not (self.__eq__(other))
 
     def __float__(self):
         return self.get_total() / 100
@@ -24,11 +21,28 @@ class ShoppingCart:
     def __getitem__(self, item):
         return [str(self.products[item]), self.quantities[item]]
 
-    def add_product(self, product, quantity):
-        self.products.append(product)
-        self.quantities.append(quantity)
+    def add_product(self, product, quantity) -> None:
+        try:
+            index = self.products.index(product)
+            self.quantities[index] += quantity
+        except ValueError:
+            self.products.append(product)
+            self.quantities.append(quantity)
 
-    def get_total(self):
+    def remove_product(self, product) -> None:
+        return self.products.remove(product)
+
+    def sub_product(self, product, quantity) -> None:
+        try:
+            index = self.products.index(product)
+            self.quantities[index] -= quantity
+            if self.quantities[index] <= 0:
+                self.products.remove(product)
+        except ValueError:
+            self.products.append(product)
+            self.quantities.append(quantity)
+
+    def get_total(self) -> int:
         total = 0
         for product, quantity in zip(self.products, self.quantities):
             total += product.get_total(quantity)
@@ -52,10 +66,7 @@ class CodeValidator(PaymentValidator):
     def is_valid(self):
         print('Card payment processing...')
         password = str(input("Enter your password:"))
-        if self.password == password:
-            return True
-        else:
-            print("Try another password")
+        return self.password == password
 
 
 class CardPaymentProcessor(CodeValidator, PaymentProcessor):
@@ -68,14 +79,14 @@ class CardPaymentProcessor(CodeValidator, PaymentProcessor):
 
 class CashValidator(PaymentValidator):
     def is_valid(self):
-        return "Cash payment processing..."
+        return True
 
 
 class CashPaymentProcessor(CashValidator, PaymentProcessor):
     def purchase(self, cart):
-        print(self.is_valid())
-        return f"Total bill in the shopping cart {cart.get_total()/100} UAH"
-
+        if self.is_valid():
+            print("Cash payment processing...")
+            return f"Total bill in the shopping cart {cart.get_total()/100} UAH"
 
 
 class Product:
@@ -90,31 +101,35 @@ class Product:
     def __float__(self):
         return self.price/100
 
+    def __eq__(self, other):
+        return self.name == other.name and self.unit == other.unit
+
     def get_total(self, quantity) -> int:
         return round(self.price * (quantity / self.unit))
 
 
 candy = Product("candy", 1059, 0.1)
 juice = Product("juice", 3655, 1)
-sweet = Product("candy", 1059, 0.1)
+sweet = Product("sweet", 1589, 0.1)
 cart_1 = ShoppingCart()
 cart_2 = ShoppingCart()
 cart_1.add_product(candy, 1)
 cart_1.add_product(sweet, 0.5)
+cart_1.add_product(juice, 1)
+cart_1.add_product(sweet, 0.5)
 cart_2.add_product(candy, 2)
 cart_2.add_product(juice, 2)
 cart_2.add_product(sweet, 0.2)
-card_processor = CardPaymentProcessor("1234")
-card_processor.purchase(cart_1)
-cash_processor = CashPaymentProcessor()
-cash_processor.purchase(cart_1)
+# cart_1.remove_product(candy)
+# card_processor = CardPaymentProcessor("1234")
+# card_processor.purchase(cart_1)
+# cash_processor = CashPaymentProcessor()
+# cash_processor.purchase(cart_1)
 
-# print(str(juice))
-# print(float(juice))
-# print(cart_1.get_total())
-# print(float(cart_1))
-# print(bool(cart_1))
-# print(cart_1 == cart_2)
-print(len(cart_2))
-print(cart_1[0])
+
+# print(cart_1.sub_product(candy, 1))
+print(cart_1.get_total())
+
+
+
 
